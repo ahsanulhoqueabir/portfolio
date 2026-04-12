d28ca2bef10433784833dd97a882e704058a045d
ahsanulhoqueabir <contact.ahsanul@gmail.com>
2026-04-12 13:09:47 +0600

    feat: add certification model schema and export

---
+++ models/certification.m.ts
@@ -0,0 +1,44 @@
+import { model, models, Schema, type InferSchemaType } from "mongoose";
+
+const certificationSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    issuer: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    issueDate: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    images: {
+      type: [String],
+      required: true,
+      default: [],
+    },
+    credential: {
+      type: String,
+      trim: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    collection: "certifications",
+    timestamps: true,
+  },
+);
+
+export type CertificationDocument = InferSchemaType<typeof certificationSchema>;
+
+export const CertificationModel =
+  models.Certification ||
+  model<CertificationDocument>("Certification", certificationSchema);
