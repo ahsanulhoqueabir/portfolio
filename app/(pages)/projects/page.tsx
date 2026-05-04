093e233e6262ec0e52a6496836aa724e73368c2d
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2026-05-05 03:07:41 +0600

    Delete app directory

--- app/(pages)/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectsPageContent from "@/components/projects-page/projects-page-content";
-import { getSiteContext } from "@/lib/site-context";
-
-export default async function ProjectsPage() {
-  const siteContext = await getSiteContext();
-  const { projects } = siteContext.projects;
-
-  return <ProjectsPageContent projects={projects} />;
-}
